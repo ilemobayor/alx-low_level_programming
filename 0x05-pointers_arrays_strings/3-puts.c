89a585aa5f66e17878feab42ad537e862616ab92
ilemobayor <ajikanilemobayor@gmail.com>
2022-09-19 10:11:28 -0700

    task 3

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ *_puts- prints out to stdout with new line
+ *@str: checked
+ *Return:nothing
+ */
+void _puts(char *str)
+{
+	if (str)
+	{
+		while (*str)
+		{
+			_putchar(*str++);
+		}
+	}
+	_putchar('\n');
+
+}
