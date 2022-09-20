bd451fee66439df511eb51cba4d5cffc4246dc77
ilemobayor <ajikanilemobayor@gmail.com>
2022-09-20 03:14:08 -0700

    task 11

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -16,22 +16,18 @@ int main(void)
 	for (i = 0; i < 4; i++)
 	{
 
-		pass[3 * i] = '0' + (rand() % 10);
+	pass[3 * i] = '0' + (rand() % 10);
+	char capLetter = 'A' + (rand() % 26);
 
-		char capLetter = 'A' + (rand() % 26);
+	pass[(3 * i) + 1] = capLetter;
+	char letter = 'a' + (rand() % 26);
 
-		pass[(3 * i) + 1] = capLetter;
-
-		char letter = 'a' + (rand() % 26);
-
-		pass[(3 * i) + 2] = letter;
+	pass[(3 * i) + 2] = letter;
 	}
 
 	pass[3 * i] = '\0';
-
 	printf("generated password : %s\n\n", pass);
 
 	printf("\n\n");
-
 	return (0);
 }
