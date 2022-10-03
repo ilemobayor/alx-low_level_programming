2d7693351f36bfd01780e73c065e8e4bcabf9e3f
ilemobayor <ajikanilemobayor@gmail.com>
2022-10-03 11:21:17 -0700

    task 4

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -3,18 +3,21 @@
 
 /**
  * _atoi - converts a string to an integer
- * @s: string to be converte
+ * @s: string to be converted
+ *
  * Return: the int converted from the string
  */
 int _atoi(char *s)
 {
 	int i, d, n, len, f, digit;
+
 	i = 0;
 	d = 0;
 	n = 0;
 	len = 0;
 	f = 0;
 	digit = 0;
+
 	while (s[len] != '\0')
 		len++;
 
@@ -22,6 +25,7 @@ int _atoi(char *s)
 	{
 		if (s[i] == '-')
 			++d;
+
 		if (s[i] >= '0' && s[i] <= '9')
 		{
 			digit = s[i] - '0';
@@ -30,16 +34,17 @@ int _atoi(char *s)
 			n = n * 10 + digit;
 			f = 1;
 			if (s[i + 1] < '0' || s[i + 1] > '9)
-				break
+				break;
 			f = 0;
 		}
 		i++;
 	}
 
 	if (f == 0)
-			return (0);
+		return (0);
 
 	return (n);
+
 }
 
 /**
@@ -47,17 +52,16 @@ int _atoi(char *s)
  * @argc: number of arguments
  * @argv: array of arguments
  * Return: 0 (Success), 1 (Error)
-
-*/
+ */
 int main(int argc, char *argv[])
 {
 	int result, num1, num2;
 
-	if (argc < 3 || argc > 3)
+	if (argc < 3 || argc > 3;)
 	{
 
-		printf("Error\n")
-		return (1)
+		printf("Error\n");
+		return (1);
 
 	}
 
